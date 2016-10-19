30781054bedd2db18777c99f8eab21feb86a7186
forestlzj <xpelive@yahoo.com.cn>
2016-10-19 19:13:44 +0800

    Create CsvFileFormatException.java

---
+++ stockloader/src/main/java/com/forest/algotrade/stockloader/router/CsvFileFormatException.java
@@ -0,0 +1,11 @@
+package com.forest.algotrade.stockloader.router;
+
+/**
+ * Created by pdclzj on 19/10/2016.
+ */
+public class CsvFileFormatException extends FileForamtException {
+
+    public CsvFileFormatException(String s) {
+        super(s);
+    }
+}
