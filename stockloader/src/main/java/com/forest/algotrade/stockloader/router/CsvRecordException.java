f2b6db1f63e204374976013f9c003fc2d05f3cf1
forestlzj <xpelive@yahoo.com.cn>
2016-10-19 19:18:04 +0800

    Create CsvRecordException.java

---
+++ stockloader/src/main/java/com/forest/algotrade/stockloader/router/CsvRecordException.java
@@ -0,0 +1,11 @@
+package com.forest.algotrade.stockloader.router;
+
+/**
+ * Created by pdclzj on 19/10/2016.
+ */
+public class CsvRecordException extends RecordException {
+
+    public CsvRecordException(String s) {
+        super(s);
+    }
+}
